f0752ac170d29fda1acba1660cfb0dcfa7bb87fb
knight.gao <knightgao2@gmail.com>
2023-04-06 22:03:19 +0800

    feat: 新增版本

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version, VERSION_NEUTRAL } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: 'user',
+  version: '1',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // http://localhost:3000/v1/user 或者 http://localhost:3000/user
   @Get()
-  getHello(): string {
+  @Version([VERSION_NEUTRAL, '1'])
+  findAll() {
     return this.appService.getHello();
+  }
+
+  // http://localhost:3000/v2/user
+  @Get()
+  @Version('2')
+  findAll2() {
+    return 'i am new one';
   }
 }
