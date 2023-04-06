038bdab0e39b4f41b78b311391516d8422ab5fe6
knight.gao <knightgao2@gmail.com>
2023-04-06 22:11:10 +0800

    feat: 处理异常

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/platform-fastify';
 import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -21,6 +23,9 @@ async function bootstrap() {
 
   // 统一返回体
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  // 异常过滤
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
 
   await app.listen(3000);
 }
